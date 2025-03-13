3f60127a9b1d0c3e4a32bdb5265d55bd41e7d625
JakeWilliamsCode <106281755+JakeWilliamsCode@users.noreply.github.com>
2025-03-13 09:27:29 -0500

    Create Contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,17 @@
+import NavigationBar from "../Components/NavigationBar";
+import Title from "../Components/Title";
+import AboutCard from "../Components/AboutCard";
+import Resume from "../Components/Resume/Resume";
+import { Card } from "react-bootstrap";
+function Contact() {
+  return (
+    <div data-bs-theme="dark" class ="background">
+        <NavigationBar></NavigationBar>
+      <Card>
+      <Card.Title>Under Construction</Card.Title>
+      </Card>
+    </div>
+  );
+}
+
+export default Contact;
