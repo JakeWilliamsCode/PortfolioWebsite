9c0d259d2df552af91e92ac873f4af508d61351c
JakeWilliamsCode <Jakewill51503@gmail.com>
2026-01-04 17:37:07 -0600

    Switched from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import './App.css'
-import Hobbies from './Components/Hobbies';
-import Projects from './Components/Projects';
-import Home from './Components/Home';
-import Contact from './Components/Contact';
-import ResumePDF from './Components/ResumePDF';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Hobbies from "./Components/Hobbies";
+import Projects from "./Components/Projects";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import ResumePDF from "./Components/ResumePDF";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/PortfolioWebsite" element={<Home/>}/>
-      <Route path="/Projects" element={<Projects/>}/>
-      <Route path="/PDFResume" element={<ResumePDF/>}/>
-      <Route path="/Hobbies" element ={<Hobbies/>}/>
-      <Route path="/Contact" element = {<Contact/>}/>
-    </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<HashRouter>
+			<Routes>
+				<Route path="/PortfolioWebsite" element={<Home />} />
+				<Route path="/Projects" element={<Projects />} />
+				<Route path="/PDFResume" element={<ResumePDF />} />
+				<Route path="/Hobbies" element={<Hobbies />} />
+				<Route path="/Contact" element={<Contact />} />
+			</Routes>
+		</HashRouter>
+	);
 }
 
-export default App
+export default App;
